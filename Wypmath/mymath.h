ba500ceead74f2b7c01a08eafbba5d4d1dd780f1
Yupingwu <pingw@cqu.edu.cn>
2020-03-20 17:10:33 +0800

    wyp first commit

---
+++ Wypmath/mymath.h
@@ -0,0 +1,22 @@
+/*
+ * mymath.h
+ *
+ *  Created on: 2020年3月20日
+ *      Author: hi
+ */
+
+#ifndef MYMATH_H_
+#define MYMATH_H_
+
+
+class Mymath{
+private:
+	double sign=-1;
+
+public:
+	double mysin (double x);
+	double mycos (double x);
+};
+
+
+#endif /* MYMATH_H_ */
